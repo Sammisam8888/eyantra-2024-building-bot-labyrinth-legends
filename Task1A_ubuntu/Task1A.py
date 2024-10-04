ca827b13dbee9c0b0ce5d379aeb3845074e969d9
Trishna2005Das <trishnadas2005@gmail.com>
2024-10-04 21:25:49 +0530

    lqr gain added

--- Task1A_ubuntu/Task1A.py
+++ Task1A_ubuntu/Task1A.py
@@ -1,104 +1,100 @@
 import sympy as sp
-import numpy as np 
+import numpy as np
+import control
 
-########## Initialization of  variables & provided  
+########## Initialization of variables & provided  
 # Define the symbolic variables
-x1, x2, u = +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++sp.symbols('x1 x2 u')
+x1, x2, u = sp.symbols('x1 x2 u')
 
 # Define the differential equations
-x1_dot = sp.symbol('x1_dot')
-x2_dot = sp.symbol('x2_dot')
+x1_dot = sp.symbols('x1_dot')
+x2_dot = sp.symbols('x2_dot')
 ##################################################
 
-#initialisation of equation 
-eq1= sp.Eq(x1_dot, -x1+2*x1**3+x2+4*u)
-eq2=sp.Eq(x2_dot, -x1-x2+2*u)
+# Initialization of equation
+eq1 = sp.Eq(x1_dot, -x1 + 2 * x1 ** 3 + x2 + 4 * u)
+eq2 = sp.Eq(x2_dot, -x1 - x2 + 2 * u)
 
 
 def find_equilibrium_points():
     '''
     1. Substitute input(u) = 0 in both equation for finding equilibrium points 
     2. Equate x1_dot, x2_dot equal to zero for finding equilibrium points 
-    3. solve the x1_dot, x2_dot equations for the unknown variables and save the value to the variable namely "equi_points"
+    3. Solve the x1_dot, x2_dot equations for the unknown variables and save the value to the variable namely "equi_points"
     '''
-    eq1_sub=eq1.subs(u,0)
-    eq2_sub=eq2.subs(u,0)
-    x1_dot=0
-
-    eq11=sp.Eq(eq1_sub,0)
-    eq21=sp.Eq(eq2_sub,0)
-    equi_points = sp.solve([eq11,eq21],(x1,x2))
-    ###### WRITE YOUR CODE HERE ################
     
-    ############################################
+    eq1_sub = eq1.subs(u, 0)
+    eq2_sub = eq2.subs(u, 0)
+
+    eq11 = sp.Eq(eq1_sub, 0)
+    eq21 = sp.Eq(eq2_sub, 0)
+    
+    equi_points = sp.solve([eq11, eq21], (x1, x2))
 
     return equi_points
 
-def find_A_B_matrices( eq_points):
+
+def find_A_B_matrices(eq_points):
     '''
-    1. Substitute every equilibrium points that you have already find in the find_equilibrium_points() function 
+    1. Substitute every equilibrium points that you have already found in the find_equilibrium_points() function 
     2. After substituting the equilibrium points, Save the Jacobian matrices A and B as A_matrices, B_matrices  
     '''
-
     A_matrix = sp.Matrix([
         [sp.diff(x1_dot, x1), sp.diff(x1_dot, x2)],
         [sp.diff(x2_dot, x1), sp.diff(x2_dot, x2)]
     ])
-    
+
     B_matrix = sp.Matrix([
         [sp.diff(x1_dot, u)],
         [sp.diff(x2_dot, u)]
     ])
+
     A_matrices, B_matrices = [], []
-    
-    ###### WRITE YOUR CODE HERE ################
-    for t in enumerate(eq_points,1):
-    	i,point=t
-    	eq1_sub_point=A_matrix.subs({x1: point[x1],x2: point[x2]})
-    	eq2_sub_point=B_matrix.subs({x1: point[x1],x2: point[x2]})
-    	A_matrices.append(eq1_sub_point)
-    	B_matrices.append(eq2_sub_point)
-    	
-    ############################################
-    
-    return A_matrices,B_matrices
+
+    for i, point in enumerate(eq_points, 1):
+        A_sub = A_matrix.subs({x1: point[x1], x2: point[x2]})
+        B_sub = B_matrix.subs({x1: point[x1], x2: point[x2]})
+        A_matrices.append(A_sub)
+        B_matrices.append(B_sub)
+
+    return A_matrices, B_matrices
 
 
 def find_eigen_values(A_matrices):
     '''
-    1.  Find the eigen values of all A_matrices (You can use the eigenvals() function of sympy) 
-        and append it to the 'eigen_values' list
-    2.  With the eigen values, determine whether the system is stable or not and
-        append the string 'Stable' if system is stable, else append the string 'Unstable'
-        to the 'stability' list 
+    1. Find the eigen values of all A_matrices (You can use the eigenvals() function of sympy) 
+       and append it to the 'eigen_values' list
+    2. With the eigen values, determine whether the system is stable or not and
+       append the string 'Stable' if system is stable, else append the string 'Unstable'
+       to the 'stability' list 
     '''
-    
     eigen_values = []
     stability = []
 
-    ###### WRITE YOUR CODE HERE ################
     for a in A_matrices:
-    	eigval=a.eigenvals()
-    	eigen_values.append(eigval)
-    	stable = True
-    	for eig in eigval.keys():
-	    if (eig.as_real_img()[0]>=0): 
-#eig.as_real_img() - it converts real+imaginary(3+2i) to (real,imaginary) 
-	    	stable=False
-	    	break
-	if (stable):
-	  stability.append('Stable')
-	else:
-	   stability.append('Unstable')
-    ############################################
+        eigval = a.eigenvals()
+        eigen_values.append(eigval)
+        
+        stable = True
+        for eig in eigval.keys():
+            if eig.as_real_imag()[0] >= 0:  # eig.as_real_imag() converts (real+imaginary) to (real, imaginary)
+                stable = False
+                break
+        
+        if stable:
+            stability.append('Stable')
+        else:
+            stability.append('Unstable')
+
     return eigen_values, stability
+
 
 def compute_lqr_gain(jacobians_A, jacobians_B):
     K = 0
     '''
-    This function is use to compute the LQR gain matrix K
+    This function is used to compute the LQR gain matrix K
     1. Use the Jacobian A and B matrix at the equilibrium point (-1,1) and assign it to A and B respectively for computing LQR gain
-    2.  Compute the LQR gain of the given system equation (You can use lqr() of control module) 
+    2. Compute the LQR gain of the given system equation (You can use lqr() of control module) 
     3. Take the A matrix corresponding to the Unstable Equilibrium point (-1,1) that you have already found for computing LQR gain.
     4. Assign the value of gain to the variable K
     '''
@@ -107,12 +103,17 @@ def compute_lqr_gain(jacobians_A, jacobians_B):
     Q = np.eye(2)  # State weighting matrix
     R = np.array([1])  # Control weighting matrix
 
-    ###### WRITE YOUR CODE HERE ################
-    
-    ############################################
+    A = jacobians_A[0]
+    B = jacobians_B[0]
+
+    npA = np.array(A).astype(np.float64)
+    npB = np.array(B).astype(np.float64)
+    K, _, _ = control.lqr(npA, npB, Q, R)
+
     return K
 
-def main_function(): # Don't change anything in this function 
+
+def main_function():  # Don't change anything in this function 
     # Find equilibrium points
     eq_points = find_equilibrium_points()
     
@@ -126,11 +127,10 @@ def main_function(): # Don't change anything in this function
     # For finding eigenvalues and stability of the given equation
     eigen_values, stability = find_eigen_values(jacobians_A)
     
-    
     # Compute the LQR gain matrix K
     K = compute_lqr_gain(jacobians_A, jacobians_B)
     
-    return eq_points, jacobians_A,  eigen_values, stability, K
+    return eq_points, jacobians_A, eigen_values, stability, K
 
 
 def task1a_output():
@@ -168,4 +168,3 @@ if __name__ == "__main__":
 
     # print the results
     task1a_output()
-
